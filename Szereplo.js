e334811465d039cdcf5b9a35fa499fb71e1ae757
Wendesday36 <bertazoeanna@gmail.com>
2023-04-26 09:02:12 +0200

    set,get

--- Szereplo.js
+++ Szereplo.js
@@ -1,8 +1,16 @@
 class Szereplo {
+    #szuletesinev//privát adattag
   constructor(nev,szul_ev) {
     //this. aktualis példányra mutat
     this.nev=nev
+    this.#szuletesinev=nev
     this.szul_ev=szul_ev
+  }
+  getSzuletesinev(){
+    return this.#szuletesinev;
+  }
+  setSzueltesinev(){
+    this.#szuletesinev=ujnev
   }
 }
 export default Szereplo;
--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-import  Szereplo from "./Szereplo.js";
+import Szereplo from "./Szereplo.js";
 
-
-$(function(){
-    //példányositjuk
-    const szereplo1 = new Szereplo("János",1972)
-    console.log(szereplo1)
-})
+$(function () {
+  //példányositjuk
+  const szereplo1 = new Szereplo("János", 1972);
+  console.log(szereplo1);
+  szereplo1.jegy = 12;
+  console.log(szereplo1);
+  szereplo1.nev = "Béla";
+  console.log(szereplo1);
+  //szereplo1.#szuletesinev="Béla" nem tudod változtatni mert privát
+  console.log(szereplo1.getSzuletesNev)
+});
