9e6adef82ad6cb8a98aa968daed1465d5b72a7fa
Wendesday36 <bertazoeanna@gmail.com>
2023-04-26 09:22:37 +0200

    kattintas

--- Szereplo.js
+++ Szereplo.js
@@ -1,16 +1,38 @@
 class Szereplo {
-    #szuletesinev//privát adattag
-  constructor(nev,szul_ev) {
+  #szuletesinev; //privát adattag
+  constructor(nev, szul_ev,szuloElem) {
     //this. aktualis példányra mutat
-    this.nev=nev
-    this.#szuletesinev=nev
-    this.szul_ev=szul_ev
+    this.nev = nev;
+    this.#szuletesinev = nev;
+    this.szul_ev = szul_ev;
+    this.hanyEves=this.kor()
+
+    //adatok megjelenitese a html oldalon
+    szuloElem.append(`<div class="szereplo">
+            <h3>${this.nev}</h3>
+            <p>${this.hanyEves}</p>
+    </div>`)
+    //minden olyan adattaghoz kell egy adattagot rendelni amivel kesobb foglalkozni akarunk
+    this.szereploElem=$(".szereplo:last-child")
+    //rakattintok az aktualis div-re
+    console.log(this.szereploElem)
+    this.szereploElem.on("click",function(){
+        //function esetén a this az esemény kiválto elemre mutat
+        console.log(this.nev)
+    })
   }
-  getSzuletesinev(){
+  getSzuletesinev() {
     return this.#szuletesinev;
   }
-  setSzueltesinev(){
-    this.#szuletesinev=ujnev
+  setSzueltesinev(ujnev) {
+    //csak nagy betuvel kezdodhet,nem tartalmazhat szamokat
+    this.#szuletesinev = ujnev;
+  }
+  kor() {
+    const d = new Date();
+    let year = d.getFullYear();
+    let evekSzama = year-this.szul_ev;
+    return evekSzama;
   }
 }
 export default Szereplo;
--- main.js
+++ main.js
@@ -2,12 +2,18 @@ import Szereplo from "./Szereplo.js";
 
 $(function () {
   //példányositjuk
-  const szereplo1 = new Szereplo("János", 1972);
-  console.log(szereplo1);
+  const szuloElem = $(".tarolo");
+  const szereplo1 = new Szereplo("János", 1972,szuloElem);
+  const szereplo2 = new Szereplo("Éva", 2000,szuloElem);
+ /*  console.log(szereplo1); */
+  
+
   szereplo1.jegy = 12;
-  console.log(szereplo1);
+ /*  console.log(szereplo1); */
   szereplo1.nev = "Béla";
-  console.log(szereplo1);
+  /* console.log(szereplo1); */
   //szereplo1.#szuletesinev="Béla" nem tudod változtatni mert privát
-  console.log(szereplo1.getSzuletesNev)
+  /* console.log(szereplo1.getSzuletesinev());
+  console.log(szereplo1.kor()); */
+  
 });
